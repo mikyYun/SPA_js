35cd508462ab77cf9fc9be815a3c184d24c66a51
mikyYun <mkyun2714@gmail.com>
2022-07-09 13:27:01 -0700

    paypal

--- cart.js
+++ cart.js
@@ -42,8 +42,85 @@ function buildCartObj(product) {
     return cartObj.items.findIndex(x => {
       return x.productName === item.productName && x.productPrice === item.productPrice;
     }) == index;
-  })
+  });
   cartObj.items = result;
-  // buildDOM();
-  console.log("cartObject length check",cartObj.items.length, result)
+  buildDOM();
+  // console.log("cartObject length check",cartObj.items.length, result)
 } // end buildCartObj function
+
+function buildDOM() {
+  let cartItems = cartObj.items;
+  while (cartCol.children.length) {
+    cartCol.removeChild(cartCol.children[0]);
+  }
+  // console.log(cartObj.items)
+
+
+  for (let i = 0; i < cartItems.length; i++) {
+    let div = document.createElement("div");
+    let productTitle = document.createElement("h5");
+    let image = document.createElement("img");
+    let productPrice = document.createElement("p");
+    let productId = document.createElement("p");
+    let removeBtn = document.createElement("button");
+    div.setAttribute("class", "col-sm-8 d-flex justify-content-between cart-div");
+    productTitle.innerHTML = cartItems[i].productName;
+    image.src = cartItems[i].productImg;
+    image.class = "cart-image"; // img-fluid
+    image.style.width = "20%";
+    productPrice.iNNerHTML = cartItems[i].productPrice;
+    productId.innerHTML = cartItems[i].productId;
+    removeBtn.setAttribute("class", "btn btn-danger");
+    removeBtn.innerHTML = "remove";
+    removeBtn.setAttribute("onclick", "remove(this)");
+    div.appendChild(productTitle);
+    div.appendChild(image);
+    div.appendChild(productPrice);
+    div.appendChild(productId);
+    div.appendChild(removeBtn);
+    cartCol.appendChild(div);
+    total(cartObj);
+  }
+  // console.log(div)
+  cartCOntainer.style.display = "block";
+
+} // end buildDOM function
+
+function remove(button) {
+  // console.log(button, button.parentElement, button.previousElementSibling)
+  let itemId = parseInt(button.previousElementSibling.innerHTML);
+  // console.log(itemId);
+  for (let i = 0; i < cartObj.items.length; i++) {
+    if (cartObj.items[i].productId === itemId) {
+      cartObj.items.splice(i, 1);
+    }
+  }
+  buildDOM();
+  total(cartObj);
+} // end remove function
+
+function total(cartObj) {
+  let sum = 0;
+  cartObj.items.forEach((item) => {
+    let itemPrice = parseFloat(item.productPrice.substring(1));
+    // console.log(itemPrice)
+    sum += itemPrice;
+  });
+  cartObj.total = sum;
+  document.getElementById("totalP").innerHTML = `Cart total is $${sum}`;
+  paypal();
+} // end total function
+
+function paypal() { // paypal.html <form>
+  if (cartObj.total > 0) {
+    let paypalBtn = document.createElement("button");
+    paypalBtn.setAttribute("class", "btn btn-primary btn-lg");
+    let anchor = document.createElement("a");
+    anchor.id = "anchor";
+    anchor.href = "paypal.html";
+    anchor.innerHTML = "Paypal";
+    paypalBtn.appendChild(anchor);
+    document.getElementById("totalP").appendChild(paypalBtn);
+    sessionStorage.setItem("paypalTotal", JSON.stringify(cartObj.total));
+  }
+} // end paypal function
